a5dac3ddef13adc1d1871bb01ac8cd906ea54e20
Roma595 <kotkov.roma04@mail.ru>
2025-09-15 19:02:26 +0300

    Init ToDoAppIOS

---
+++ ToDoAppIOS/ToDoAppIOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ToDoAppIOS
+//
+//  Created by Рома Котков on 15.09.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ToDoAppIOS/ToDoAppIOS/ToDoAppIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppIOSApp.swift
+//  ToDoAppIOS
+//
+//  Created by Рома Котков on 15.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
