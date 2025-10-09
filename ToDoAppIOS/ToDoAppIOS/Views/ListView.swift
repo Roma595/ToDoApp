e98ac80a4184b87197f1cfdb4d7ef13e5a362638
Roma595 <kotkov.roma04@mail.ru>
2025-10-09 19:54:17 +0300

    Add EmptyTasksView

--- ToDoAppIOS/ToDoAppIOS/Views/ListView.swift
+++ ToDoAppIOS/ToDoAppIOS/Views/ListView.swift
@@ -9,21 +9,37 @@ import SwiftUI
 import SwiftData
 
 struct ListView: View {
+    @Environment(\.modelContext) private var modelContext
+    @Query(sort: \TaskModel.name, order: .reverse) private var tasks: [TaskModel]
+    
     @State var showAddView: Bool = false
     
     var body: some View {
         NavigationStack{
-            TaskListView()
-            .navigationTitle(Text("Список задач"))
-            .toolbar{
-                ListViewToolbar(showAddView: $showAddView)
+            if (tasks.isEmpty ){
+                EmptyTasksView()
+                .toolbar{
+                    ListViewToolbar(showAddView: $showAddView)
+                }
+                .sheet(isPresented: $showAddView) {
+                    AddTaskView()
+                        .interactiveDismissDisabled(true)
+                }
             }
-            .sheet(isPresented: $showAddView) {
-                AddTaskView()
-                    .interactiveDismissDisabled(true)
+            else{
+                TaskListView()
+                .navigationTitle(Text("Список задач"))
+                .toolbar{
+                    ListViewToolbar(showAddView: $showAddView)
+                }
+                .sheet(isPresented: $showAddView) {
+                    AddTaskView()
+                        .interactiveDismissDisabled(true)
+                }
+                .background(Color(.systemGray6)
+                    .ignoresSafeArea())
             }
-            .background(Color(.systemGray6)
-                .ignoresSafeArea())
+            
         }
     }
 }
---
+++ ToDoAppIOS/ToDoAppIOS/Views/Tasks/EmptyTasksView.swift
@@ -0,0 +1,18 @@
+//
+//  EmptyTasksView.swift
+//  ToDoAppIOS
+//
+//  Created by Рома Котков on 09.10.2025.
+//
+
+import SwiftUI
+
+struct EmptyTasksView: View {
+    var body: some View {
+        Text("У вас нет задач. Скорее добавьте свою первую задачу!")
+    }
+}
+
+#Preview {
+    EmptyTasksView()
+}
