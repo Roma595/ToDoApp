71d845b0bfce19cc7045f05a2ff1711a1741ebf4
0sobennaya <143941641+0sobennaya@users.noreply.github.com>
2025-10-11 19:57:21 +0300

    Reengineering app
    Удалено прошлое приложение. Сделана навигация с помощью NavController

--- ToDoAppANDROID/app/build.gradle.kts
+++ ToDoAppANDROID/app/build.gradle.kts
@@ -42,9 +42,12 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.fragment)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation("com.google.android.material:material:1.13.0")
     implementation("androidx.fragment:fragment-ktx:1.3.4")
+    implementation ("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation ("androidx.navigation:navigation-ui-ktx:2.7.7")
 }
--- ToDoAppANDROID/app/src/main/java/com/example/todoappandroid/MainActivity.kt
+++ ToDoAppANDROID/app/src/main/java/com/example/todoappandroid/MainActivity.kt
@@ -1,52 +1,24 @@
 package com.example.todoappandroid
 
-
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
+
 class MainActivity : AppCompatActivity() {
-    fun replaceFragment(fragment: Fragment) {
-        supportFragmentManager.beginTransaction().apply {
-            replace(R.id.container, fragment)
-            commit()
-        }
-    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        val host: NavHostFragment = supportFragmentManager.findFragmentById(R.id.navFragment) as NavHostFragment? ?: return
+        val navController = host.navController
 
-        replaceFragment(ListFragment()) // открываем список по умолчанию
-
-        bottomNavigationView.setOnItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.navigation_list -> {
-                    replaceFragment(ListFragment())
-                    true
-                }
-                R.id.navigation_calendar -> {
-                    replaceFragment(CalendarFragment())
-                    true
-                }
-                R.id.navigation_profile -> {
-                    replaceFragment(ProfileFragment())
-                    true
-                }
-                else -> false
-            }
-        }
+        val bottomBar = findViewById<BottomNavigationView>(R.id.bottom_nav_menu)
+        bottomBar.setupWithNavController(navController)
 
     }
+
 }
